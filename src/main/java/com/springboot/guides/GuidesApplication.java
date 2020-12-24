caeabb7571507eaa5d889819c1e832b963c073cc
linwenwei <linwenwei@comen.com>
2020-12-24 20:05:26 +0800

    进度:Building Java Projects with Maven

--- src/main/java/com/springboot/guides/GuidesApplication.java
+++ src/main/java/com/springboot/guides/GuidesApplication.java
@@ -1,13 +1,38 @@
 package com.springboot.guides;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
+
+import com.springboot.guides.consumingRest.Quote;
 
 @SpringBootApplication
+@EnableScheduling
 public class GuidesApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(GuidesApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(GuidesApplication.class, args);
+	}
+	
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+	
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception{
+		return args -> {
+			Quote quote = restTemplate.getForObject("https://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+			log.info(quote.toString());
+		};
 	}
 
 }
